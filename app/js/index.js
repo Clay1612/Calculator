137ab400b6fcc3f746a6e0e3e70651d3b0324799
Nikita <dedsec1612@gmail.com>
2020-11-29 15:16:59 +0300

    Калькулятор V1.0 не готовая

--- app/js/index.js
+++ app/js/index.js
@@ -1,1 +1,32 @@
-console.log('Test js!!!');
+for (let numbers of document.querySelectorAll('.number')) {
+
+  numbers.addEventListener("click" , rem);
+}
+
+function rem(event) {
+  let a = event.target.innerHTML;
+
+  let result = document.querySelector('.calculator__result');
+  result.innerHTML += a;
+ }
+
+for (let oper of document.querySelectorAll('.operator')) {
+
+  oper.addEventListener("click" , rem);
+}
+
+
+
+
+
+
+// let a = document.getElementById('7');
+// let b = document.getElementById('8');
+
+// let c = +a.innerHTML + +b.innerHTML;
+
+// console.log(c);
+
+// for (let numbers of document.querySelectorAll('.number')) {
+
+// }
