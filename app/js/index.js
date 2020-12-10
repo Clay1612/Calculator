1c84e78871a76e49fb9994b0619554a36aa518ad
Nikita <dedsec1612@gmail.com>
2020-12-10 09:05:18 +0300

    новая версия калькулятора. Нужно решить проблему чисел и хранения истории

--- app/js/index.js
+++ app/js/index.js
@@ -1,49 +1,112 @@
-"use strict"
+'use strict'
 
-function getHistory() {
-  return document.querySelector('.calculator__history').innerHTML;
+//Global variable
+let values = [];
+let hist = document.querySelector('.calculator__history');
+let result = document.querySelector('.calculator__result');
+
+let val = [];
+
+//Print on History Display
+let display = function() {
+	hist.innerHTML = values.join("");
 }
 
-function printHistory(num) {
-  return document.querySelector('.calculator__history').innerHTML = num;
+// Arithmetic functions
+let Arithmetic = {
+  summary : function sum(a , b) {return a + b},
+  subtract : function sub(a , b) {return a - b},
+  multiply : function mul(a , b) {return a * b},
+  divide : function div(a , b) {return a / b},
+};
+
+//Event Listeners
+for(let number of document.querySelectorAll('.number')) {
+  number.addEventListener('click' , function () {
+    values.push(this.innerHTML);
+    display();
+
+    console.log(values);
+  })
 }
 
-function getResult() {
-  return document.querySelector('.calculator__result').value;
+for(let operator of document.querySelectorAll('.operator')) {
+  operator.addEventListener('click' , function () {
+    // val = values.join('');
+    // val.push(this.innerHTML);
+    values.push(this.innerHTML);
+    display();
+    console.log(values);
+  })
 }
 
-function printResult(num) {
-  return document.querySelector('.calculator__result').value = num;
-}
+// Reset and Compute
+let reset = document.querySelector('.functionality__reset-button');
+reset.addEventListener('click' , function(){
+  hist.innerHTML = '';
+  result.value = '';
+  values = [];
+})
 
-for (let operator of document.querySelectorAll(".operator")) {
-  operator.addEventListener("click", function () {
-    if(this.id == "Reset") {
-      printHistory("");
-      printResult("");
-    } else {
-        let result = getResult();
-        let history = getHistory();
-        if(result!="") {
-          history = history+result;
-          if(this.id=="=") {
-            let res = eval(history);
-            printResult(res);
-            printHistory("");
-          } else {
-            history=history+this.id;
-            printHistory(history);
-            printResult("");
-          }
-        }
-    }
-  });
-}
+let compute = document.querySelector('.functionality__compute-button');
+compute.addEventListener('click', function() {
+  if (values[1] === '+') {
+    result.value = Arithmetic.summary(Number(values[0]) , Number(values[2]));
+  } else if (values[1] === '-') {
+    result.value = Arithmetic.subtract(Number(values[0]) , Number(values[2]));
+  } else if (values[1] === '*') {
+    result.value = Arithmetic.multiply(Number(values[0]) , Number(values[2]));
+  } else if (values[1] === '/') {
+    result.value = Arithmetic.divide(Number(values[0]) , Number(values[2]));
+  }
+})
 
-for (let number of document.querySelectorAll(".number")) {
-  number.addEventListener("click", function () {
-    let res = getResult();
-    res=res+this.id;
-    printResult(res);
-  });
-}
+//Previos version of Calculator
+
+// function getHistory() {
+//   return document.querySelector('.calculator__history').innerHTML;
+// }
+
+// function printHistory(num) {
+//   return document.querySelector('.calculator__history').innerHTML = num;
+// }
+
+// function getResult() {
+//   return document.querySelector('.calculator__result').value;
+// }
+
+// function printResult(num) {
+//   return document.querySelector('.calculator__result').value = num;
+// }
+
+// for (let operator of document.querySelectorAll('.operator')) {
+//   operator.addEventListener('click', function () {
+//     if(this.id === 'Reset') {
+//       printHistory('');
+//       printResult('');
+//     } else {
+//         let result = getResult();
+//         let history = getHistory();
+//         if(result) {
+//           history = history+result;
+//           if(this.id === '=') {
+//             let res = eval(history);
+//             printResult(res);
+//             printHistory('');
+//           } else {
+//             history = history + this.id;
+//             printHistory(history);
+//             printResult('');
+//           }
+//         }
+//     }
+//   });
+// }
+
+// for (let number of document.querySelectorAll('.number')) {
+//   number.addEventListener('click', function () {
+//     let res = getResult();
+//     res = res + this.id;
+//     printResult(res);
+//   });
+// }
